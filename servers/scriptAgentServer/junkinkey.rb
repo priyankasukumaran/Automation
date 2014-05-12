ac174ea7130c13e58d71b576a57a09e7c70d198f
root <root@WS1A079.(none)>
2014-05-12 14:29:06 +0530

    eee

---
+++ servers/scriptAgentServer/junkinkey.rb
@@ -0,0 +1,16 @@
+Your public key has been saved in key.pub.
+The key fingerprint is:
+04:b9:aa:ab:c2:bf:76:f7:a0:0a:08:bb:79:38:e0:ec root@WS1A079
+The key's randomart image is:
++--[ DSA 1024]----+
+|      ..         |
+|      ..         |
+|       ..        |
+|      ..         |
+|.    .  S        |
+|+.  .            |
+|Bo .   .         |
+|+*+. ....        |
+|*E+=+.. ..       |
++-----------------+
+
